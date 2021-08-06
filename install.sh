ec4d098402706d5e5e43ccb629c034d3833b931c
Ian Hill <kamarja@users.noreply.github.com>
2021-08-06 06:24:51 -0400

    added terraform install function

--- install.sh
+++ install.sh
@@ -8,3 +8,27 @@ echo "BitOps uses alpine linux as its base, so you'll want to use apk commands (
 echo ""
 
 apk info
+
+export TERRAFORM_VERSIONS=$(cat build.config.yaml | shyaml get-values terraform.versions)
+export HELM_VERSION=$(cat build.config.yaml | shyaml get-value helm.version)
+export KUBECTL_VERSION=$(cat build.config.yaml | shyaml get-value kubectl.version)
+export CLOUD_PLATFORM=$(cat build.config.yaml | shyaml get-value cloud_platform.name)
+export CI_PLATFORM=$(cat build.config.yaml | shyaml get-value ci_platform.name)
+export AWS_REGION=$(cat build.config.yaml | shyaml get-value cloud_platform.region)
+export CURRENT_ENVIRONMENT=$(cat build.config.yaml | shyaml get-value environment.default)
+
+
+mkdir -p /opt/download
+cd /opt/download
+
+function install_terraform() {
+    while IFS='' read -r version; do
+        TERRAFORM_DOWNLOAD_URL="https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip"
+        echo ${TERRAFORM_DOWNLOAD_URL}
+        curl -LO ${TERRAFORM_DOWNLOAD_URL} && unzip terraform_${version}_linux_amd64.zip -d ./
+        mv terraform /usr/local/bin/terraform-${version}
+        chmod +x /usr/local/bin/terraform-${version}
+    done <<< "$TERRAFORM_VERSIONS"
+}
+
+install_terraform
